e1f6a95db57656ed3d31619d9e2502cf3ba60400
GuilhermeC18 <52559295+GuilhermeC18@users.noreply.github.com>
2021-03-03 13:40:12 +0100

    update state

--- src/App.js
+++ src/App.js
@@ -12,25 +12,27 @@ function App() {
   const [logMessages, setLogMessages] = useState([]);
   const [counter, setCounter] = useState();
  
+  useEffect(() =>{
     connection.onopen = function(session, details) {
       session.call('com.filmdatabox.democontrol.journal')
         .then(function showSum(res) {
           setLogMessages(res);
       }, session.log);
       const onCounter = function(args) {
-        console.log(args[0])
+        setLogMessages(logMessages ,args[0])
      }
 
       session.subscribe("com.filmdatabox.democontrol.journal", onCounter)
       .then(function addLog(res){
+        console.log(res, "res");
         setLogMessages( logMessages, res)
       })
-    }
+    }}, [setLogMessages]);
  
-  
+  console.log(logMessages);
   return (
    <>
-    {logMessages? logMessages.map((m, key) => <p>{m}</p>) : <div></div>}
+    {logMessages !== [] ? logMessages.map((m, key) => <p>{m}</p>) : <div></div>}
    </>
   );
 }
