5901f053ff9c369f5c79d237affc9c77e38b27c0
GuilhermeC18 <52559295+GuilhermeC18@users.noreply.github.com>
2021-03-03 10:45:31 +0100

    fetch

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState} from 'react';
 const autobahn = require('autobahn');
 
 const connection = new autobahn.Connection({
@@ -8,10 +9,25 @@ const connection = new autobahn.Connection({
 connection.open();
 
 
+
 function App() {
+  const [logMessages, setLogMessages] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(()=>{
+    fetch('com.filmdatabox.democontrol.journal')
+      .then(res => res.json())
+      .then((logMessages) =>{
+        console.log(logMessages)
+        setLogMessages(logMessages)
+      })
+      .catch((err) => setError(err));
+  }, []);
+  console.log(logMessages);
+
   return (
    <>
-    
+     
    </>
   );
 }
